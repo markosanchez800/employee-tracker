1d7661192911eb646d3793810fa8be5bd5094616
markosanchez800 <markosanchez800@gmail.com>
2021-04-24 16:18:37 -0700

    made all tables

--- prompts.js
+++ prompts.js
@@ -3,4 +3,17 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
+   
+    post: 3306,
+   
+    user:'root',
+   
+    password:'password',
+   
+    database:'employees_DB',
+});
+
+connection.connect((err)=>{
+    if(err)throw(err);
+    
 })
